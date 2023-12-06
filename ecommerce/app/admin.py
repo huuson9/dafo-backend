7162e0010e54892cfb020fed08eb90da8cc0ecc6
huuson9 <106168787+huuson9@users.noreply.github.com>
2023-12-06 11:07:35 +0700

    add table to admin

--- ecommerce/app/admin.py
+++ ecommerce/app/admin.py
@@ -12,14 +12,31 @@ from .models import (
     Voucher,
 )
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "average_rating", "price", "category", "supplier")
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("product", "user", "rating", "content", "time")
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("user", "product", "count", "time", "price")
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("user", "product", "count", "time", "price")
+
+
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Supplier)
 admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Purchase)
-admin.site.register(Cart)
+admin.site.register(Cart, CartAdmin)
 admin.site.register(ProductImage)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(Voucher)
